4150757a7a7581e2d48a4da0cfed45f88d32c820
AbijithS-aot <abijith.s@aot-technologies.com>
2022-06-06 14:53:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useEffect, useState } from "react"
+import { Form } from "react-formio"
+import axios from 'axios';
+
+const App =({src})=>{
+    
+    const [url ,setUrl] =useState('')
+
+    useEffect(()=>{
+        console.log("url in app",src)
+        const newUrl = document.querySelector('react-wc').getAttribute('url')
+        setUrl(newUrl)
+    },[src])
+    return(
+        <div>
+            <h1>{url}</h1>
+            <Form 
+            src={url}
+            onSubmit={(data)=>{
+                axios.post("http://206.116.106.143:5000/public/application/create",{formId:data.form,formUrl:`http://206.116.106.143:5000/form/${data.form}/submission/${data._id}`, submissionId:data._id})
+            }}
+            />
+        </div>
+    )
 }
 
-export default App;
+
+export default App
